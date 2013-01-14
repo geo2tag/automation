d613cf66288ebf2d8e32a00275e7d64f97684dbd
Kirill Krinkin <kirill.krinkin@gmail.com>
2013-01-15 00:04:03 +0400

    GT-810 --jobs

--- bin/build_package.sh
+++ bin/build_package.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-MAKEFLAGS+=--jobs=5
-
+alias make="make --jobs=5"
 
 ./auto/bin/update_build_number.sh
 dch --force-bad-version --local .`hostname`-`cat /opt/files/build`. "Built on `uname -a` at `date`"
