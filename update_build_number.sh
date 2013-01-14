8c81607cfa2d7c41a57a4547b8fdd227f8562812
Kirill Krinkin <kirill.krinkin@gmail.com>
2013-01-14 12:37:28 +0400

    GT-812

--- update_build_number.sh
+++ update_build_number.sh
@@ -1,10 +1,20 @@
 #!/bin/sh
 
-rm -rf /opt/files/*
+DIR="/opt/files"
 
-touch /opt/files/build
-BUILD="`cat /opt/files/build`+1|bc"
-echo ${BUILD} >/opt/files/build
+rm -rf ${DIR}/*.deb
+
+
+
+if [ ! -e ${DIR}/build ]
+then 
+	echo "0" >/opt/files/build
+fi
+
+OLD_BUILD=`cat ${DIR}/build`
+BUILD=`echo ${OLD_BUILD} +1 | bc`
+echo "New build is ${BUILD}"
+echo ${BUILD} >${DIR}/build
 
 
 
