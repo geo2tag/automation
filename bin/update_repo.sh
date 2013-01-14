033e73bb6cf9348c2e89890f76631c19f793d18f
Kirill Krinkin <kirill.krinkin@gmail.com>
2013-01-14 15:01:20 +0400

    GT-812

--- bin/update_repo.sh
+++ bin/update_repo.sh
@@ -3,6 +3,12 @@
 
 INCOMING=/opt/repo/incoming
 
+echo "# Package deploying info:"
+echo "  - Incoming folder: ${INCOMING}"
+echo "  - Build atrifacts list -"
+ls -la ../
+
+
 cp ../*.dsc     ${INCOMING}/
 cp ../*.tar.gz  ${INCOMING}/
 cp ../*.deb gz  ${INCOMING}/
