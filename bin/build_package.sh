e269dd564ac53b4159e22e486da182ced957d08d
Kirill Krinkin <kirill.krinkin@gmail.com>
2013-01-15 00:51:55 +0400

    GT-810 fail with jobs

--- bin/build_package.sh
+++ bin/build_package.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 ./auto/bin/update_build_number.sh
-dch --force-bad-version --local .`hostname`-`cat /opt/files/build`. "Built on `uname -a` at `date`"
+dch --force-bad-version --increment --local .`hostname`-`cat /opt/files/build`. "Built on `uname -a` at `date`"
 dpkg-buildpackage -rfakeroot -j 10 
