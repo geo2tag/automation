37eee27245ab1f54cebc15ea3b0d6ea970f72053
Kirill Krinkin <kirill.krinkin@gmail.com>
2013-01-15 18:19:37 +0400

    GT-812 added keyid

--- bin/build_package.sh
+++ bin/build_package.sh
@@ -3,4 +3,4 @@
 ./auto/bin/update_build_number.sh
 
 dch --force-bad-version --local .`hostname`-`cat /opt/files/build`. "Built on `uname -a` at `date`"
-dpkg-buildpackage -rfakeroot -j10 -k 38B1DDB7 
+dpkg-buildpackage -rfakeroot -j10 -k38B1DDB7 
