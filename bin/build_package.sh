2df71d8706ff13c123d7f15c71e3ee2068d5edbd
Kirill Krinkin <kirill.krinkin@gmail.com>
2013-01-15 16:51:20 +0400

    GT-812 git@

--- bin/build_package.sh
+++ bin/build_package.sh
@@ -2,5 +2,5 @@
 
 ./auto/bin/update_build_number.sh
 
-dch --force-bad-version --increment --local .`hostname`-`cat /opt/files/build`. "Built on `uname -a` at `date`"
+dch --force-bad-version --local .`hostname`-`cat /opt/files/build`. "Built on `uname -a` at `date`"
 dpkg-buildpackage -rfakeroot -j10 
