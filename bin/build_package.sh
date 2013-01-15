a7cbf20d3259c62cc893228b452e127fe9f865da
Kirill Krinkin <kirill.krinkin@gmail.com>
2013-01-15 10:34:41 +0400

    GT-810

--- bin/build_package.sh
+++ bin/build_package.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 ./auto/bin/update_build_number.sh
+
 dch --force-bad-version --increment --local .`hostname`-`cat /opt/files/build`. "Built on `uname -a` at `date`"
 dpkg-buildpackage -rfakeroot -j10 
