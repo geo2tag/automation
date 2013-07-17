675b56532552500e26234270f95bd39a75e79d1d
Mark Zaslavskiy <mark.zaslavskiy@gmail.com>
2013-07-17 22:52:42 +0400

    fixed apply_repo_updates.sh

--- bin/apply_repo_updates.sh
+++ bin/apply_repo_updates.sh
@@ -2,12 +2,12 @@
 
 if [ $# -ne "1" ]
 then
-  echo "Usage: ./apply_repo_updates.sh [testing|stable]"
-  exit 1
+	echo "Usage: ./apply_repo_updates.sh [testing|stable]"
+	exit 1
 fi
 
 SUBREPO=$1
 
-echo "Do: ssh zmm@build64.geo2tag.org cd /opt/repo/ && sudo ./import_packages.sh ${SUBREPO}"
+echo "Do: ssh zmm@build64.geo2tag.org sudo /opt/repo/import_packages.sh ${SUBREPO}"
 
-ssh zmm@build64.geo2tag.org cd /opt/repo/ && sudo ./import_packages.sh ${SUBREPO}
+ssh zmm@build64.geo2tag.org sudo /opt/repo/import_packages.sh ${SUBREPO}
