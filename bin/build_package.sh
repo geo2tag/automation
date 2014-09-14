d23f90f3fbc6f76400ed2300020d8e66f03c9d67
Kirill Krinkin <kirill.krinkin@gmail.com>
2014-09-14 16:50:00 +0200

    GT-1122 Added Source format

--- bin/build_package.sh
+++ bin/build_package.sh
@@ -2,9 +2,24 @@
 
 ./auto/bin/update_build_number.sh
 
+package="geo2tag"
+version="0.33"
+build=`cat /opt/files/build`
+
+echo "Starting build ${package} version ${version} build ${build}"
+
+rm -rf ./.build 
+mkdir -p ./.build/${package}-${version}/src/
+
+cp -r ./*   ./.build/${package}-${version}/
 
 echo `git rev-parse HEAD` >./HEAD
 
+cd ./.build/${package}-${version}/
 
-dch --local .`cat /opt/files/build`.`hostname` "Auto build at `date` `time` on `hostname` "
+#dch --local .`cat /opt/files/build`.`hostname` "Auto build at `date` `time` on `hostname` "
 dpkg-buildpackage -rfakeroot -j10 -us -uc 
+
+cd -
+
+
